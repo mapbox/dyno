78b3226e5bc4ffad671a6909119194b214fdda8b
Mick Thompson <dthompson@gmail.com>
2014-07-14 17:46:38 -0700

    extend config object, not replace it

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var _ = require('underscore');
 module.exports = function(c) {
     var dyno = {};
     dyno.config = require('./lib/config')(c);
-    dyno = _(dyno).extend(dyno.config.dynamo);
-    dyno = _(dyno).extend(require('./lib/item'));
-    dyno = _(dyno).extend(require('./lib/table'));
+    _(dyno).extend(dyno.config.dynamo);
+    _(dyno).extend(require('./lib/item'));
+    _(dyno).extend(require('./lib/table'));
     return dyno;
 }
