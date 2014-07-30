4ee37d79a870dd6c88ac2b5bcb64da3bc5d5399d
Tom MacWright <tom@macwright.org>
2014-07-30 15:36:04 -0400

    Add .putItems call. Refs #6

---
+++ test/test.batch.js
@@ -0,0 +1,38 @@
+var test = require('tap').test;
+var s = require('./setup');
+var es = require('event-stream');
+var dyno = s.dyno;
+
+test('setup', s.setup());
+test('setup table', s.setupTable);
+test('putItems', function(t) {
+    var items = randomItems(10);
+
+    dyno.putItems(items, itemResp);
+    function itemResp(err, resp) {
+        t.equal(err, null);
+        t.deepEqual(resp, {
+            UnprocessedItems: {}
+        });
+        t.end();
+        // dyno.getItem(item, getItem);
+    }
+    function getItem(err, resp) {
+        // t.equal(err, null);
+        // t.deepEqual(resp, {Item:{id: 'yo', range: 5}});
+        // t.end();
+    }
+});
+test('teardown', s.teardown);
+
+function randomItems(n) {
+    var items = [];
+    for (var i = 0; i < n; i++) {
+        items.push({
+            id: 'id:' + i.toString(),
+            range: i,
+            data: (new Array(100)).join(' '),
+        });
+    }
+    return items;
+}
