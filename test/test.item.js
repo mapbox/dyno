96702548fb53da98549ccb09a7fc9e0b9e6d1848
Mick Thompson <dthompson@gmail.com>
2014-09-17 16:59:57 -0700

    better conditional handling fixes #17 and more updateItem tests

--- test/test.item.js
+++ test/test.item.js
@@ -45,52 +45,16 @@ test('conditional put', function(t) {
         t.equal(err, null);
         t.deepEqual(resp, {});
 
-        var options = { expected: {} };
-        options.expected.range = {
-            ComparisonOperator: 'NE',
-            AttributeValueList: [ { N: item.range.toString() } ]
+        var options = {
+            expected:{
+                range:{'NE': [ { N: item.range.toString() } ]}
+            }
         };
         dyno.putItem(item, options, expectFailure)
     }
     function expectFailure(err, resp) {
         t.ok(err, 'throws an error');
         t.equal(err.code, 'ConditionalCheckFailedException', 'expected error');
-        t.end();
-    }
-});
-test('teardown', s.teardown);
-
-test('setup', s.setup());
-test('setup table', s.setupTable);
-test('update', function(t) {
-    var item = {put:{str: 'a', num: 12}, add:{count:1}};
-    var key = {id:'yo', range:5}
-
-    dyno.updateItem(key, item, itemResp);
-    function itemResp(err, resp) {
-        t.equal(err, null);
-        t.deepEqual(resp, {});
-        dyno.getItem(key, getItem);
-    }
-    function getItem(err, resp) {
-        t.equal(err, null);
-        t.deepEqual(resp, {Item:{id: 'yo', range: 5, str: 'a', num:12, count:1}});
-        t.end();
-    }
-});
-test('update - delete', function(t) {
-    var item = {delete:{str: 'a', num: 12}};
-    var key = {id:'yo', range:5}
-
-    dyno.updateItem(key, item, itemResp);
-    function itemResp(err, resp) {
-        t.equal(err, null);
-        t.deepEqual(resp, {});
-        dyno.getItem(key, getItem);
-    }
-    function getItem(err, resp) {
-        t.equal(err, null);
-        t.deepEqual(resp, {Item:{id: 'yo', range: 5, count:1}});
         t.end();
     }
 });
