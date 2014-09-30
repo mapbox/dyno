3d0620ef8c8127597486b7929694a4c3e8fa60ce
Ryan Clark <ryan.clark.j@gmail.com>
2014-09-30 15:49:32 -0700

    tests for throughput throttling [live test]

--- test/live.tests.js
+++ test/live.tests.js
@@ -3,16 +3,9 @@ var s = require('./setup')(true);
 require('./test.batch');
 require('./test.convertTypes');
 require('./test.item');
+require('./test.throughput');
 
 // Don't run table create/list/delete tests live.
 // require('./test.table');
 
-var test = require('tap').test;
-
-test('delete Table', { timeout: Infinity }, function(t) {
-    s.dyno.deleteTable(s.tableName, function(err, resp){
-        t.ifError(err, 'deleted table ' + s.tableName);
-        t.deepEqual(resp, {});
-        t.end();
-    });
-});
+s.test('delete table', s.deleteTable);
