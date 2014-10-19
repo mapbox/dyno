ad32867796520ac609b24425f9eba2ec0c126f66
Ryan Clark <ryan.clark.j@gmail.com>
2014-10-19 16:04:10 -0700

    sketch config isolation

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var _ = require('underscore');
 
 module.exports = function(c) {
     var dyno = {};
-    dyno.config = require('./lib/config')(c);
-    _(dyno).extend(dyno.config.dynamo);
-    _(dyno).extend(require('./lib/item'));
-    _(dyno).extend(require('./lib/query'));
-    _(dyno).extend(require('./lib/scan'));
-    _(dyno).extend(require('./lib/table'));
-    _(dyno).extend(require('./lib/batch'));
+    var config = require('./lib/config')(c);
+    _(dyno).extend(config.dynamo);
+    _(dyno).extend(require('./lib/item')(config));
+    _(dyno).extend(require('./lib/query')(config));
+    _(dyno).extend(require('./lib/scan')(config));
+    _(dyno).extend(require('./lib/table')(config));
+    _(dyno).extend(require('./lib/batch')(config));
     return dyno;
 };
