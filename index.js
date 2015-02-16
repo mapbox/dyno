6d2907110c62b54d3b671858a477a37f5e0b700f
Ryan Clark <ryan.clark.j@gmail.com>
2015-02-16 18:58:54 -0500

    sketch multi + kinesis

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var _ = require('underscore');
 
-module.exports = function(c) {
+module.exports = Dyno;
+
+function Dyno(c) {
     var dyno = {};
     var config = require('./lib/config')(c);
     _(dyno).extend(config.dynamo);
@@ -11,4 +13,22 @@ module.exports = function(c) {
     _(dyno).extend(require('./lib/batch')(config));
     _(dyno).extend(require('./lib/describe')(config));
     return dyno;
+}
+
+Dyno.multi = function(readConfig, writeConfig) {
+    if (!readConfig.region) throw badconfig('You must specify a read region');
+    if (!readConfig.table) throw badconfig('You must specify a read table');
+    if (!writeConfig.region) throw badconfig('You must specify a write region');
+    if (!writeConfig.table) throw badconfig('You must specify a write table');
+
+    var read = Dyno(readConfig);
+    var write = Dyno(writeConfig);
+
+    return require('./lib/multi')(read, write);
 };
+
+function badconfig(message) {
+    var err = new Error(message);
+    err.code = 'EBADCONFIG';
+    return err;
+}
