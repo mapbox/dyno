ac6e29eb91579b51500bcba88717f78c42a746cd
Will White <will@mapbox.com>
2015-06-03 14:05:54 -0400

    Remove unnecessary call to types.typesFromDynamo().

--- test/test.convertTypes.js
+++ test/test.convertTypes.js
@@ -170,14 +170,6 @@ test('typesFromDynamo - single item', function(t) {
     t.end();
 });
 
-test('typesFromDynamo - single item, no property name', function(t) {
-    var items = { S: 'id:2' };
-
-    item = types.typesFromDynamo(items);
-    t.deepEqual(item, [{ S: 'id:2' }]);
-    t.end();
-});
-
 test('typesFromDynamo - item', function(t) {
     var items = [{
         id: { S: 'id:2' },
