9a1f5fc6b99e546f1efee083b99622b19fe358bb
Ryan Clark <ryan.clark.j@gmail.com>
2015-06-08 15:39:36 -0700

    explicit table deletion in teardown

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 require('./setup')();
-require('./test.batch');
 require('./test.convertTypes');
 require('./test.cli');
 require('./test.item');
 require('./test.table');
 require('./test.kinesis');
-require('./test.multi');
+require('./test.batch');
 require('./test.config');
 require('./test.serialization');
+require('./test.multi');
