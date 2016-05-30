65544cb00e2d6b8170d3d62af240972e04c4a40f
Ryan Clark <ryan.clark.j@gmail.com>
2016-05-30 16:08:35 -0700

    adds autopagination to .query and .scan

---
+++ test/paginated.test.js
@@ -0,0 +1,297 @@
+var test = require('tape');
+var testTables = require('./test-tables');
+var dynamodb = require('dynamodb-test')(test, 'dyno', testTables['idhash-numrange']);
+var crypto = require('crypto');
+var _ = require('underscore');
+var Dyno = require('..');
+
+var fixtures = _.range(2345).map(function(i) {
+  return {
+    id: 'id',
+    num: i,
+    data: crypto.randomBytes(5 * 1024)
+  };
+});
+
+dynamodb.test('[paginated] query, 0 pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.query({
+    ExpressionAttributeNames: { '#id': 'id' },
+    ExpressionAttributeValues: { ':id': 'id' },
+    KeyConditionExpression: '#id = :id',
+    Pages: 0
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    assert.deepEqual(data, {
+      Items: [],
+      Count: 0,
+      ScannedCount: 0
+    }, 'null result');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] query, 1 page', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.query({
+    ExpressionAttributeNames: { '#id': 'id' },
+    ExpressionAttributeValues: { ':id': 'id' },
+    KeyConditionExpression: '#id = :id',
+    Pages: 1
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    var items = fixtures.slice(0, 203);
+    assert.deepEqual(data, {
+      Items: items,
+      Count: items.length,
+      ScannedCount: items.length,
+      LastEvaluatedKey: { id: 'id', num: 202 },
+      ConsumedCapacity: undefined
+    }, 'one page of results');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] query, undefined Pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.query({
+    ExpressionAttributeNames: { '#id': 'id' },
+    ExpressionAttributeValues: { ':id': 'id' },
+    KeyConditionExpression: '#id = :id'
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    var items = fixtures.slice(0, 203);
+    assert.deepEqual(data, {
+      Items: items,
+      Count: items.length,
+      ScannedCount: items.length,
+      LastEvaluatedKey: { id: 'id', num: 202 }
+    }, 'one page of results');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] query, null Pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.query({
+    ExpressionAttributeNames: { '#id': 'id' },
+    ExpressionAttributeValues: { ':id': 'id' },
+    KeyConditionExpression: '#id = :id',
+    Pages: null
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    var items = fixtures.slice(0, 203);
+    assert.deepEqual(data, {
+      Items: items,
+      Count: items.length,
+      ScannedCount: items.length,
+      LastEvaluatedKey: { id: 'id', num: 202 }
+    }, 'one page of results');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] query, 2 pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.query({
+    ExpressionAttributeNames: { '#id': 'id' },
+    ExpressionAttributeValues: { ':id': 'id' },
+    KeyConditionExpression: '#id = :id',
+    Pages: 2,
+    ReturnConsumedCapacity: 'TOTAL'
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    var items = fixtures.slice(0, 406);
+    assert.deepEqual(data, {
+      Items: items,
+      Count: items.length,
+      ScannedCount: items.length,
+      LastEvaluatedKey: { id: 'id', num: 405 },
+      ConsumedCapacity: { CapacityUnits: 255, TableName: dynamodb.tableName }
+    }, 'two pages of results');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] query, Infinity pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.query({
+    ExpressionAttributeNames: { '#id': 'id' },
+    ExpressionAttributeValues: { ':id': 'id' },
+    KeyConditionExpression: '#id = :id',
+    Pages: Infinity
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    assert.deepEqual(data, {
+      Items: fixtures,
+      Count: fixtures.length,
+      ScannedCount: fixtures.length,
+      LastEvaluatedKey: undefined,
+      ConsumedCapacity: undefined
+    }, 'Infinity pages of results');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] scan, 0 pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.scan({
+    Pages: 0
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    assert.deepEqual(data, {
+      Items: [],
+      Count: 0,
+      ScannedCount: 0
+    }, 'null result');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] scan, 1 page', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.scan({
+    Pages: 1
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    var items = fixtures.slice(0, 203);
+    assert.deepEqual(data, {
+      Items: items,
+      Count: items.length,
+      ScannedCount: items.length,
+      LastEvaluatedKey: { id: 'id', num: 202 },
+      ConsumedCapacity: undefined
+    }, 'one page of results');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] scan, undefined Pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.scan(function(err, data) {
+    if (err) return assert.end(err);
+    var items = fixtures.slice(0, 203);
+    assert.deepEqual(data, {
+      Items: items,
+      Count: items.length,
+      ScannedCount: items.length,
+      LastEvaluatedKey: { id: 'id', num: 202 }
+    }, 'one page of results');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] scan, null Pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.scan({
+    Pages: null
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    var items = fixtures.slice(0, 203);
+    assert.deepEqual(data, {
+      Items: items,
+      Count: items.length,
+      ScannedCount: items.length,
+      LastEvaluatedKey: { id: 'id', num: 202 }
+    }, 'one page of results');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] scan, 2 pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.scan({
+    Pages: 2,
+    ReturnConsumedCapacity: 'TOTAL'
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    var items = fixtures.slice(0, 406);
+    assert.deepEqual(data, {
+      Items: items,
+      Count: items.length,
+      ScannedCount: items.length,
+      LastEvaluatedKey: { id: 'id', num: 405 },
+      ConsumedCapacity: { CapacityUnits: 255, TableName: dynamodb.tableName }
+    }, 'two pages of results');
+    assert.end();
+  });
+});
+
+dynamodb.test('[paginated] scan, Infinity pages', fixtures, function(assert) {
+  var dyno = Dyno({
+    table: dynamodb.tableName,
+    region: 'local',
+    endpoint: 'http://localhost:4567'
+  });
+
+  dyno.scan({
+    Pages: Infinity
+  }, function(err, data) {
+    if (err) return assert.end(err);
+    assert.deepEqual(data, {
+      Items: fixtures,
+      Count: fixtures.length,
+      ScannedCount: fixtures.length,
+      LastEvaluatedKey: undefined,
+      ConsumedCapacity: undefined
+    }, 'Infinity pages of results');
+    assert.end();
+  });
+});
+
+dynamodb.close();
